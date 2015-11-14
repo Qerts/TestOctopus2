728f72af0d06769baf6ef7649101356c74215165
Qerts <michal.balicky@gmail.com>
2015-11-14 19:02:49 +0100

    init

---
+++ Vox/Vox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
